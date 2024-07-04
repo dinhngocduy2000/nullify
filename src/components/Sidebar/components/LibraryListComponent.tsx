783ee88c93627229ed1a082d4b58f11ec5cd1f76
ngocduydinh <ngocduydinh2000@gmail.com>
2024-07-04 13:26:20 +0700

    adding react query to list library component

--- src/components/Sidebar/components/LibraryListComponent.tsx
+++ src/components/Sidebar/components/LibraryListComponent.tsx
@@ -1,27 +1,36 @@
-import { URL_ENUM } from "@/library/enum/url-enum";
 import { fetchFollowedArtist, fetchUserTopItems } from "@/networking/usersAPI";
 import { PlusIcon } from "@heroicons/react/20/solid";
 import { Square3Stack3DIcon } from "@heroicons/react/24/outline";
 import React from "react";
-import LinkItem from "./LinkItem";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/library/enum/query-keys";
+import ListLibraryItems from "./ListLibraryItems";
 
 type Props = {};
-async function getFollowedArtists() {
-  return await fetchFollowedArtist();
-}
+const queryClient = new QueryClient();
+const prefetchFollowedArtists = async () => {
+  await queryClient.prefetchQuery({
+    queryKey: [QUERY_KEYS.FOLLOWED_ARTIST],
+    queryFn: fetchFollowedArtist,
+  });
+};
 
-async function getTopAlbums() {
-  return await fetchUserTopItems("tracks");
-}
-
+const prefetchTopAlbums = async () => {
+  await queryClient.prefetchQuery({
+    queryKey: [QUERY_KEYS.TOP_ALBUMS],
+    queryFn: () => fetchUserTopItems("tracks"),
+  });
+};
 const LibraryListComponent = async (props: Props) => {
-  const [followedArtists, topAlbums] = await Promise.all([
-    getFollowedArtists(),
-    getTopAlbums(),
+  const [] = await Promise.all([
+    prefetchFollowedArtists(),
+    prefetchTopAlbums(),
   ]);
-  console.log("====================================");
-  console.log(topAlbums);
-  console.log("====================================");
+
   return (
     <div className="relative w-full flex-1 overflow-hidden rounded-lg bg-default pb-4 hover:overflow-auto">
       <div className="sticky top-0 z-10 flex w-full items-center justify-between bg-default p-4 pr-2">
@@ -31,29 +40,9 @@ const LibraryListComponent = async (props: Props) => {
         </div>
         <PlusIcon className="box-border h-[30px] min-h-[30px] w-[30px] min-w-[30px] rounded-full bg-opacity-10 p-1 text-default hover:cursor-pointer hover:bg-[#2b2b2b] hover:text-white" />
       </div>
-      <div className="flex-1 overflow-auto px-2">
-        {followedArtists.artists.items.map((artist) => (
-          <LinkItem
-            key={artist.id}
-            id={artist.id}
-            href={URL_ENUM.ARTIST + `/${artist.id}`}
-            src={artist.images[artist.images.length - 1].url}
-            name={artist.name}
-            type={artist.type}
-          />
-        ))}
-        {topAlbums.items.map((album) => (
-          <LinkItem
-            key={album.id}
-            id={album.id}
-            href={URL_ENUM.ALBUMS + `/${album.id}`}
-            src={album.album.images[album.album.images.length - 1].url}
-            name={album.name}
-            type={album.album.type}
-            artist={album.artists[0].name}
-          />
-        ))}
-      </div>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <ListLibraryItems />
+      </HydrationBoundary>
     </div>
   );
 };
---
+++ src/components/Sidebar/components/ListLibraryItems.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { QUERY_KEYS } from "@/library/enum/query-keys";
+import { URL_ENUM } from "@/library/enum/url-enum";
+import { fetchFollowedArtist, fetchUserTopItems } from "@/networking/usersAPI";
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import LinkItem from "./LinkItem";
+
+type Props = {};
+
+const ListLibraryItems = (props: Props) => {
+  const { data: followedArtists } = useQuery({
+    queryKey: [QUERY_KEYS.FOLLOWED_ARTIST],
+    queryFn: fetchFollowedArtist,
+  });
+  const { data: topAlbums } = useQuery({
+    queryKey: [QUERY_KEYS.TOP_ALBUMS],
+    queryFn: () => fetchUserTopItems("tracks"),
+  });
+
+  return (
+    <div className="flex-1 overflow-auto px-2">
+      {followedArtists?.artists.items.map((artist) => (
+        <LinkItem
+          key={artist.id}
+          id={artist.id}
+          href={URL_ENUM.ARTIST + `/${artist.id}`}
+          src={artist.images[artist.images.length - 1].url}
+          name={artist.name}
+          type={artist.type}
+        />
+      ))}
+      {topAlbums?.items.map((album) => (
+        <LinkItem
+          key={album.id}
+          id={album.id}
+          href={URL_ENUM.ALBUMS + `/${album.id}`}
+          src={album.album.images[album.album.images.length - 1].url}
+          name={album.name}
+          type={album.album.type}
+          artist={album.artists[0].name}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ListLibraryItems;
