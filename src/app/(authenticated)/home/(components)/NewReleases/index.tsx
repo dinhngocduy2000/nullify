e199161b07410c719780e05bc0f595a1ec360274
ngocduydinh <ngocduydinh2000@gmail.com>
2024-07-10 16:09:59 +0700

    create list items for reuseable

--- src/app/(authenticated)/home/(components)/NewReleases/index.tsx
+++ src/app/(authenticated)/home/(components)/NewReleases/index.tsx
@@ -1,3 +1,4 @@
+import ListSpotifyItems from "@/components/ListSpotifyItems";
 import NoDataComponent from "@/components/NoData";
 import SpotifyItemComponent from "@/components/SpotifyItem";
 import { getNewReleases } from "@/networking/AlbumApi";
@@ -13,34 +14,23 @@ const NewReleasesComponent = async (props: Props) => {
   console.log("====================================");
 
   return (
-    <div className="max-w-full">
-      <div className="mb-2 flex w-full justify-between">
-        <p className="mb-2 text-xl font-semibold">New Releases Albums</p>
-        <Link
-          href={"#"}
-          className="text-sm font-semibold text-gray-400 hover:underline"
-        >
-          Show all
-        </Link>
-      </div>
-      <div className="flex max-w-full gap-4 overflow-x-hidden pb-4 hover:overflow-x-auto">
-        {!newReleases.albums ? (
-          <NoDataComponent />
-        ) : (
-          newReleases?.albums?.items?.map((album) => (
-            <SpotifyItemComponent
-              key={album.id}
-              href={`/albums/${album.id}`}
-              src={album.images[0].url}
-              height={album.images[2].height}
-              width={album.images[2].width}
-              title={album.name}
-              subTitle={album.artists.map((artist) => artist.name).join(", ")}
-            />
-          ))
-        )}
-      </div>
-    </div>
+    <ListSpotifyItems title={"New Release Albums"} showAllUrl={"#"}>
+      {!newReleases.albums ? (
+        <NoDataComponent />
+      ) : (
+        newReleases?.albums?.items?.map((album) => (
+          <SpotifyItemComponent
+            key={album.id}
+            href={`/albums/${album.id}`}
+            src={album.images[0].url}
+            height={album.images[2].height}
+            width={album.images[2].width}
+            title={album.name}
+            subTitle={album.artists.map((artist) => artist.name).join(", ")}
+          />
+        ))
+      )}
+    </ListSpotifyItems>
   );
 };
 
---
+++ src/components/ListSpotifyItems/index.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import React, { ReactNode } from "react";
+
+type Props = {
+  title: string;
+  showAllUrl: string;
+  children: ReactNode;
+};
+
+const ListSpotifyItems = ({ children, title, showAllUrl }: Props) => {
+  return (
+    <div className="max-w-full">
+      <div className="mb-2 flex w-full justify-between">
+        <p className="mb-2 text-xl font-semibold">{title}</p>
+        <Link
+          href={showAllUrl}
+          className="text-sm font-semibold text-gray-400 hover:underline"
+        >
+          Show all
+        </Link>
+      </div>
+      <div className="flex max-w-full gap-4 overflow-x-hidden pb-4 hover:overflow-x-auto">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default ListSpotifyItems;
