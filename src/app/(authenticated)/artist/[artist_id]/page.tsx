72bbda3b12f6bc92048c13f7e37d31c4728448fb
Dylan Dinh <ngocduydinh2000@gmail.com>
2024-08-11 23:16:57 +0700

    resolve vercel issues

--- src/app/(authenticated)/artist/[artist_id]/page.tsx
+++ src/app/(authenticated)/artist/[artist_id]/page.tsx
@@ -1,3 +1,4 @@
+import NoDataComponent from "@/components/NoData";
 import { getArtistDetail } from "@/networking/ArtistApi";
 import { Metadata, ResolvingMetadata } from "next";
 import React, { cache } from "react";
@@ -10,22 +11,18 @@ type Props = {
 const handleFetchArtistDetails = cache(async (id: string) => {
   return await getArtistDetail(id);
 });
-export async function generateMetadata(
-  { params }: Props,
-  parent: ResolvingMetadata,
-): Promise<Metadata> {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   // read route params
 
   // fetch data
   const artistDetails = await handleFetchArtistDetails(params.artist_id);
 
   // optionally access and extend (rather than replace) parent metadata
-  const previousImages = (await parent).openGraph?.images || [];
 
   return {
-    title: `Nullify - ${artistDetails.name}`,
+    title: `Nullify - ${artistDetails?.name}`,
     openGraph: {},
-    description: `Details about ${artistDetails.name}`,
+    description: `Details about ${artistDetails?.name}`,
   };
 }
 const ArtistDetailPage = async ({ params }: Props) => {
@@ -34,7 +31,10 @@ const ArtistDetailPage = async ({ params }: Props) => {
   console.log("====================================");
   console.log(artistDetails);
   console.log("====================================");
-  return <div>{artistDetails.name}</div>;
+  if (!artistDetails) {
+    return <NoDataComponent />;
+  }
+  return <div>{artistDetails?.name}</div>;
 };
 
 export default ArtistDetailPage;
